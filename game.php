081d777f308148738875a606ac9c98bb3e9c67aa
Aivaras Jastrumskis <aivarasj12@gmail.com>
2023-12-12 16:11:32 +0200

    game session update

--- game.php
+++ game.php
@@ -1,23 +1,29 @@
 <?php
+session_start();
+
 $player1 = "";
 $player2 = "";
 $score1 = 0;
 $score2 = 0; 
-$currentPlayer = 1;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     if (isset($_POST["start"])) {
         $player1 = $_POST["player1"];
         $player2 = $_POST["player2"];
-    } elseif (isset($_POST["roll"])) {
+        $_SESSION["player1"] = $player1;
+        $_SESSION["player2"] = $player2;
+        $_SESSION["score1"] = 0;
+        $_SESSION["score2"] = 0;
+        $_SESSION["currentPlayer"] = 1;
+    } elseif (isset($_POST["roll"]) && !empty($_SESSION["player1"]) && !empty($_SESSION["player2"])) {
         $diceResult = rand(1, 6);
-        ${"score" . $currentPlayer} += $diceResult;
-        $currentPlayer = 3 - $currentPlayer;
+        ${"score" . $_SESSION["currentPlayer"]} += $diceResult;
+        $_SESSION["currentPlayer"] = 3 - $_SESSION["currentPlayer"];
     }
 }
 
-if ($score1 >= 30 || $score2 >= 30) {
-    $winner = ($score1 >= 30) ? $player1 : $player2;
+if ($_SESSION["score1"] >= 30 || $_SESSION["score2"] >= 30) {
+    $winner = ($_SESSION["score1"] >= 30) ? $_SESSION["player1"] : $_SESSION["player2"];
     echo "<p>Sveikiname! $winner laimėjo!</p>";
 }
 ?>
@@ -30,20 +36,20 @@ if ($score1 >= 30 || $score2 >= 30) {
     <title>Game</title>
 </head>
 <body>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <?php if (empty($player1) || empty($player2)) : ?>
-            <label for="player1">Player 1 Name:</label>
-            <input type="text" name="player1" required>
-            <label for="player2">Player 2 Name:</label>
-            <input type="text" name="player2" required>
-            <button type="submit" name="start">Start Game</button>
-        <?php else : ?>
-            <p><?php echo "Current player: Player $currentPlayer - {${"player" . $currentPlayer}}"; ?></p>
-            <p>Player 1 Score: <?php echo $score1; ?></p>
-            <p>Player 2 Score: <?php echo $score2; ?></p>
-            <button type="submit" name="roll">Roll the Dice</button>
-        <?php endif; ?>
-    </form>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <?php if (empty($_SESSION["player1"]) || empty($_SESSION["player2"])) : ?>
+        <label for="player1">Player 1 Name:</label>
+        <input type="text" name="player1" value="<?php echo $player1; ?>" required>
+        <label for="player2">Player 2 Name:</label>
+        <input type="text" name="player2" value="<?php echo $player2; ?>" required>
+        <button type="submit" name="start">Start Game</button>
+    <?php else : ?>
+        <p><?php echo "Current player: Player " . $_SESSION["currentPlayer"] . " - " . ${"player" . $_SESSION["currentPlayer"]}; ?></p>
+        <p>Player 1 Score: <?php echo $_SESSION["score1"]; ?></p>
+        <p>Player 2 Score: <?php echo $_SESSION["score2"]; ?></p>
+        <button type="submit" name="roll">Roll the Dice</button>
+    <?php endif; ?>
+</form>
 </body>
 <!-- 11. papildomas
 Suprogramuokite žaidimą. Žaidimas prasideda dviem laukeliais, kuriuose žaidėjai įveda savo vardus ir mygtuku “pradėti”. 
